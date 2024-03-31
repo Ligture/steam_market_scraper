284eb4f4480dd94a41dcc7203ae1e2cc80cde816
Ligture <2073377971@qq.com>
2024-03-31 13:30:52 +0800

    1.0.0

--- inspectlink.py
+++ inspectlink.py
@@ -1,37 +1,40 @@
 from bs4 import BeautifulSoup
 import json
-g_rgAssets = ''
-class Assets():
+
+g_rgAssets = ""
+
+
+class Assets:
     def __init__(self, html):
-        self.g_rgAssets = ''
+        self.g_rgAssets = ""
         self.html = html
         self.all_inspectlinks = []
         self.initial()
 
     def initial(self):
-        soup = BeautifulSoup(self.html, 'html.parser')
-        a = soup.find_all('script')
-        g_rgAssets1 = ''
+        soup = BeautifulSoup(self.html, "html.parser")
+        a = soup.find_all("script")
+        g_rgAssets1 = ""
         for i in a:
-            if 'g_rgAssets' in i.text:
-                start = i.text.find('g_rgAssets')
-                end = i.text.find('g_rgCurrency')
-                g_rgAssets1 = i.text[start + 13:end - 4].strip().strip(';')
+            if "g_rgAssets" in i.text:
+                start = i.text.find("g_rgAssets")
+                end = i.text.find("g_rgCurrency")
+                g_rgAssets1 = i.text[start + 13 : end - 4].strip().strip(";")
         self.g_rgAssets = g_rgAssets1
         print(self.g_rgAssets)
         j = json.loads(self.g_rgAssets)
         print(j)
         self.all_inspectlinks = []
-        for i in j['730']['2']:
+        for i in j["730"]["2"]:
             print(i)
-            self.all_inspectlinks.append((i,j['730']['2'][i]['actions'][0]['link']))
+            self.all_inspectlinks.append((i, j["730"]["2"][i]["actions"][0]["link"]))
         print(self.all_inspectlinks)
 
     def getinspectlinkbyid(self, id):
         for i in self.all_inspectlinks:
             if i[1].find(id) != -1:
                 assetid = i[0]
-                url = i[1].replace('%assetid%', assetid)
+                url = i[1].replace("%assetid%", assetid)
                 return url
             else:
                 return None
@@ -40,6 +43,6 @@ class Assets():
         links = []
         for i in self.all_inspectlinks:
             assetid = i[0]
-            url = i[1].replace('%assetid%', assetid)
+            url = i[1].replace("%assetid%", assetid)
             links.append(url)
         return links
--- steamscraper.py
+++ steamscraper.py
@@ -8,55 +8,83 @@ import json
 proxy = {
     #'http': 'socks5://127.0.0.1:7890',
     #'https': 'socks5://127.0.0.1:7890'
-    #"http": "http://10.10.1.10:3128",
-    #"https": "http://10.10.1.10:1080",
-
+    # "http": "http://10.10.1.10:3128",
+    # "https": "http://10.10.1.10:1080",
 }
+
+
 def parser(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    childitem = soup.find_all(class_='market_listing_item_img_container')
-    item_ids = []
+    soup = BeautifulSoup(html, "html.parser")
+    childitem = soup.find_all(class_="market_listing_item_img_container")
+
+    # 格式:{item_id: price}
+
+    items = {}
     for i in range(len(childitem)):
         child: bs4.Tag = childitem[i]
-        id = child.find(class_='market_listing_item_img')
-        id1 = id.get('id')
+        id = child.find(class_="market_listing_item_img")
+
+        id1 = id.get("id")
         try:
-            if id1[0:7] == 'listing':
+            if id1[0:7] == "listing":
                 item_id = id1[0:-6]
-                item_ids.append(item_id)
+                price = child.parent.find(
+                    class_="market_listing_right_cell market_listing_their_price"
+                ).find("span", class_="market_listing_price_with_fee")
+                price = price.getText(strip=True)
+
+                items[item_id] = price
         except BaseException as e:
             print(e)
-    print(item_ids)
-    return item_ids
+    print(items)
+    return items
 
-def getfloat(inslink):
-    data = requests.get(f'http://127.0.0.1/?url={inslink}')
+
+def getfloat(inslink, price, unit="CNY"):
+    currency = {
+        "₴": "UAH",
+        "¥": "CNY",
+        "₸": "KZT",
+        "₪": "ILS",
+        "$": "USD",
+        "zł": "PLN",
+        "NT$": "TWD",
+        "₩": "KRW",
+        "pуб.": "RUB",
+        "CHF": "CHF",
+        'kr': 'SEK',
+        'A$': 'AUD',
+        'R$': 'BRL',
+        '₫': 'VND'
+    }
+    data = requests.get(f"http://127.0.0.1/?url={inslink}")
     data = json.loads(data.text)
     print(data)
-    float = data['iteminfo']['floatvalue']
-    paintseed = data['iteminfo']['paintseed']
-    assetid = data['iteminfo']['a']
-    return assetid, float, paintseed
+    float = data["iteminfo"]["floatvalue"]
+    paintseed = data["iteminfo"]["paintseed"]
+    assetid = data["iteminfo"]["a"]
+    itemid = data["iteminfo"]["m"]
+    return assetid, float, paintseed, price["listing_" + itemid]
 
 
-checkurl = 'https://steamcommunity.com/market/listings/730/MP5-SD%20%7C%20Liquidation%20%28Field-Tested%29'
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'}
-html = requests.get(checkurl,
-                    headers=headers,
-                    proxies=proxy)
+query = {"start": 0, "count": 100}  # 不知道作用  # 10 25 50 100
+checkurl = "https://steamcommunity.com/market/listings/730/MP5-SD%20%7C%20Liquidation%20%28Field-Tested%29"
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+    "accept-language": "zh-cn",
+}
+html = requests.get(checkurl, headers=headers, proxies=proxy, params=query)
 if html.status_code != 200:
-    print('失败')
+    print("失败")
 else:
     item_info = []
-    item_ids = parser(html.text)
+
+    # {item_id:price}
+    items = parser(html.text)
     ins = inspectlink.Assets(html.text)
     links = ins.getallinspectlinks()
     for i in links:
-        item_info.append(getfloat(i))
-    item_info.sort(key=lambda x:x[1])
+        item_info.append(getfloat(i, items))
+
+    item_info.sort(key=lambda x: x[1])
     print(item_info)
-
-
-
-
-
